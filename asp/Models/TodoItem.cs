f17ed8feeffacf3ee9cd3759bf46892e4b546362
Peter-The-Great <56716150+Peter-The-Great@users.noreply.github.com>
2024-10-16 23:53:49 +0200

    Refactor WeatherForecastController and WeatherForecast class

---
+++ asp/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace asp.Models;
+
+[Table("TodoList")]
+public class TodoItem
+{
+    [Key]
+    public long Id { get; set; }
+    [Required]
+    public string? Name { get; set; }
+    [Required]
+    public bool IsComplete { get; set; }
+}
